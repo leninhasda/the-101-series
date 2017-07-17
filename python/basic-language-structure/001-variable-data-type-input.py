3d0855ad0365bb38d0c2330b2aa9bd42080e6f43
Lenin <lenin@pathao.com>
2017-07-17 13:21:01 +0600

    updated basic python learning added a mongo test

---
+++ python/basic-language-structure/001-variable-data-type-input.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# like any other language variables can be defined
+a = 10
+b = 5
+c = a + b
+print (c)
+
+# shorthand
+# right part can be a list
+x, y, z = 10, 20, 10
+print (x+y+z)
+
+# input
+# input() string return kore cast kore nite hoi
+f,g=input().split(' ')
+print(f+g)
---
+++ python/basic-language-structure/003-comments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# well this is a comment :p
+# there is no multi line comment, so have to do this way
+
+"""
+and these are called document comment
+"""
+'which can also be written as this!'
