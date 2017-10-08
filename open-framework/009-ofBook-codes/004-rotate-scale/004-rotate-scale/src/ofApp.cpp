8a6109a425de6729c2183e2f0137125c708c0d04
Lenin Hasda <leninhasda@gmail.com>
2017-10-08 09:25:41 +0600

    crazy rotation :D

--- open-framework/009-ofBook-codes/004-rotate-scale/004-rotate-scale/src/ofApp.cpp
+++ open-framework/009-ofBook-codes/004-rotate-scale/004-rotate-scale/src/ofApp.cpp
@@ -39,14 +39,23 @@ void ofApp::draw(){
 	//ofSetColor(ofColor(150, g, b));
 	ofNoFill();
 	ofPushMatrix();
+		float z = (ofGetWidth() + ofGetHeight()) / 4;
+		if (percent) {
+			z = -1 * z;
+		}
+		else {
+			z = -1 * z;
+		}
 		ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
 
 		for (int i = 0; i < 100; i++) {	
-			ofScale(1.1,1.1);
+			ofScale(1.1, 1.1, 1.1);
 			//ofRotate(5);
 			float time = ofGetElapsedTimef();
 			float noise = ofSignedNoise(time * timeScale) * 20.0;
-			ofRotate(noise);
+			ofRotateY(noise);
+			ofRotateZ(noise);
+			//ofRotate(noise);
 			ofDrawRectangle(0, 0, 50,50);
 		}
 	ofPopMatrix();
